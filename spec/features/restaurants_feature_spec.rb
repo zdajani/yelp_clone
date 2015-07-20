ab9c6f728af4137771dc0228c1de6b2ae8a2b2e9
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-07-20 19:30:58 +0100

    Added feature to delete restaurants by clicking a link

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -55,4 +55,15 @@ feature 'restaurants' do
       expect(current_path).to eq '/restaurants'
     end
   end
+
+  context 'deleting restaurants' do
+    before {Restaurant.create name: 'Nandos'}
+
+    scenario 'removes a restaurant when a user clicks a delete link' do
+      visit '/restaurants'
+      click_link 'Delete Nandos'
+      expect(page).not_to have_content 'Nandos'
+      expect(page).to have_content 'Restaurant deleted successfully'
+    end
+  end
 end
