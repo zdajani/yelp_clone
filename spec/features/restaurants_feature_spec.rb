f707cf3bd1634c2822efaf3d2664d4be2fc347a7
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-07-20 20:58:17 +0100

    Test for adding reviews. Set up reviews resource, view, model, controller files. Failing test.

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -67,3 +67,18 @@ feature 'restaurants' do
     end
   end
 end
+
+feature 'reviewing' do
+  before {Restaurant.create name: 'Nandos'}
+
+  scenario 'allows users to leave a review using a form' do
+    visit '/restaurants'
+    click_link 'Review Nandos'
+    fill_in 'Thoughts', with: 'awesome'
+    select '5', from: 'Rating'
+    click_button 'Leave Review'
+
+    expect(current_path).to eq '/restaurants'
+    expect(page).to have_content('awesome')
+  end
+end
