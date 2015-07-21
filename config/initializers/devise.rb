fd3c60e2e0173532ec1d82de768cd9dfda1c4b3f
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-07-21 14:48:50 +0100

    Set up devise and signing in with facebook functions

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -262,4 +262,5 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+  config.omniauth :facebook, "1123376507691561", "d4cac5a408701daf93aa743f237d9835"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'restaurants#index'
 
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   resources :restaurants do
     resources :reviews
