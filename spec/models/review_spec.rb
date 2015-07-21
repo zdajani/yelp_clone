afb0a0ed6682e5a96c60da280bb7787d0656bf4f
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-07-21 11:45:16 +0100

    Rating can not be more than 5

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Review, :type => :model do
+  it 'is invalid if the rating is more than 5' do
+    review = Review.new(rating: 10)
+    expect(review).to have(1).error_on(:rating)
+  end
 end
