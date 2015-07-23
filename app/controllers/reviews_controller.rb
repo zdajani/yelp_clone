415ede9feefcf4d39667f6900b1a77c6f413209e
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-07-23 11:08:54 +0100

    Reviews validate uniquness of user. Added to create method for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,8 +6,17 @@ class ReviewsController < ApplicationController
 
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @restaurant.reviews.create(review_params)
-    redirect_to restaurants_path
+    @review = @restaurant.build_review review_params, current_user
+
+    if @review.save
+      redirect_to restaurants_path
+    else
+      if @review.errors[:user]
+        redirect_to restaurants_path, alert: 'You have already reviewed this restaurant'
+      else
+        render :new
+      end
+    end
   end
 
   def review_params
--- db/migrate/20150722154555_remove_restaurant_id_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveRestaurantIdFromUsers < ActiveRecord::Migration
-  def change
-    remove_column :users, :User_Id, :string
-  end
-end
