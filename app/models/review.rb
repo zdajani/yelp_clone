b555587e09e592183700e33eef8dd57e857c47e6
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-07-23 19:15:45 +0100

    Attempting to make reviews deletable by users who created them

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,10 @@ class Review < ActiveRecord::Base
   belongs_to :user
   validates :rating, inclusion: (1..5)
   validates :user, uniqueness: { scope: :restaurant, message: "has reviewed this restaurant already"}
+
+  def destroy_as(user)
+    return false unless self.user == user
+    destroy
+    true
+  end
 end
--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 feature 'reviewing' do
   before do
-    user = User.create email: 'tansaku@gmail.com', password: '12345678', password_confirmation: '12345678'
-    user.restaurants.create(name: 'Nandos')
+    @user = User.create email: 'fake@gmail.com', password: '12345678', password_confirmation: '12345678'
+    @user.restaurants.create(name: 'Nandos')
   end
 
   scenario 'allows users to leave a review using a form' do
@@ -12,8 +12,19 @@ feature 'reviewing' do
     fill_in 'Thoughts', with: 'awesome'
     select '5', from: 'Rating'
     click_button 'Leave Review'
-
     expect(current_path).to eq '/restaurants'
     expect(page).to have_content('awesome')
+  end
+
+  scenario 'reviews can be deleted by the user who created them' do
+    login_as @user
+    visit '/restaurants'
+    click_link 'Review Nandos'
+    fill_in 'Thoughts', with: 'awesome'
+    select '5', from: 'Rating'
+    click_button 'Leave Review'
+    click_link 'Nandos'
+    click_link 'Delete review'
+    expect(page).not_to have_content('awesome')
   end
 end
