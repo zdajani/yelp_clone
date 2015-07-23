503deffcf6adfdd89c4eff255a6c7fc488e518e9
Zeina Dajani <dajani.zeina@gmail.com>
2015-07-23 18:05:21 +0100

    adds test for verifying destroyed restaurants

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,8 +1,19 @@
 require 'rails_helper'
 
 describe Review, :type => :model do
-  it 'is invalid if the rating is more than 5' do
+  it { should belong_to(:restaurant) }
+  
+  it { should belong_to(:user) }
+
+  it "is invalid if the rating is more than 5" do
     review = Review.new(rating: 10)
     expect(review).to have(1).error_on(:rating)
   end
+
+  # it 'is removed from the database when restaurant deleted' do
+  #   restaurant = Restaurant.create(name: 'subway')
+  #   restaurant.reviews.new(thoughts: 'ok sub', rating: 3)
+  #   restaurant.destroy
+  #   expect(Review.find_by(thoughts: 'ok sub')).to eq nil
+  # end
 end
